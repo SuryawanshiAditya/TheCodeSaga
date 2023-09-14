2d0e221165389c742acaa7b6a6da653e82ed5eec
Ayush Ahirrao <ayushahirrao@gmail.com>
2023-09-14 10:30:40 +0530

    generate registration code

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -25,6 +25,7 @@ IndividualParticipationRadio.addEventListener('change', () => {
 const UPIRadio = document.querySelector('#UPIRadio');
 UPIRadio.addEventListener('change', () => {
   if (UPIRadio.checked) {
+    console.log("upi");
     document.querySelector('.upiQRContainer').style.display = "block";
     document.querySelector('.CashRegistrationIDContainer').style.display = "none";
 
@@ -33,12 +34,29 @@ UPIRadio.addEventListener('change', () => {
   }
 })
 
+const randomCode = generateRandomCode(10);
 const CashRadio = document.querySelector('#cashRadio');
+const registration_id = document.querySelector('#rid');
 CashRadio.addEventListener('change', () => {
   if (CashRadio.checked) {
+    console.log("cash");
+    registration_id.innerHTML = randomCode
     document.querySelector('.CashRegistrationIDContainer').style.display = "block";
     document.querySelector('.upiQRContainer').style.display = "none";
   } else {
     document.querySelector('.CashRegistrationIDContainer').style.display = "none";
   }
 })
+
+
+function generateRandomCode(length) {
+  const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randomCode = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    randomCode += charset[randomIndex];
+  }
+
+  return randomCode;
+}
